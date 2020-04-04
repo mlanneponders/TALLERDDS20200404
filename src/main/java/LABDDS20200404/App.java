2dc8951a3c10088699f275cb7287e156263a0f68
mlanneponders <mlyonadi@gmail.com>
2020-04-04 11:44:16 -0300

    commit inicial

---
+++ src/main/java/LABDDS20200404/App.java
@@ -0,0 +1,13 @@
+package LABDDS20200404;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/LABDDS20200404/AppTest.java
@@ -0,0 +1,20 @@
+package LABDDS20200404;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
